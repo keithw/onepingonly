123eba69f751f839e4e9f9b67045f5b39ca21c68
Keith Winstein <keithw@mit.edu>
2011-07-26 18:05:47 -0400

    Initial test

---
+++ test1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import pyaudio
+import sys
+import struct
+import math
+
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+RATE = 44100
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format = FORMAT,
+                channels = CHANNELS,
+                rate = RATE,
+                input = False,
+                output = True,
+                frames_per_buffer = 0)
+
+CHUNK = 1024
+TIME = 0 # seconds
+data = ""
+
+while True:
+    while len(data) < CHUNK:
+        value = .25 * math.sin( 440.0 * TIME / (2 * math.pi) )
+        data += struct.pack( '=f', value )
+        TIME += 1.0 / RATE
+
+    stream.write(data)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
