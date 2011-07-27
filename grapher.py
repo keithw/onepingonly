a3f14cb5bf7e7ca8e1bda1624fde2785442e6765
Keith Winstein <keithw@mit.edu>
2011-07-26 22:26:24 -0400

    480 Hz sending

--- grapher.py
+++ grapher.py
@@ -28,8 +28,11 @@ axes = colormap.alloc_color( "black" )
 
 FORMAT = pyaudio.paFloat32
 CHANNELS = 1
-RATE = 44100
-CHUNK = 4410
+RATE = 48000
+FREQ = 480
+CYCLES_PER_BIT = 5
+BIT = int(RATE*CYCLES_PER_BIT/FREQ)
+CHUNK = 32 * BIT
 
 p = pyaudio.PyAudio()
 
@@ -44,44 +47,44 @@ TIME = 0 # seconds
 
 def capture_audio(*args):
     global TIME
-    global drawable
-    global WIDTH
-    global HEIGHT
 
     cosines = []
     sines = []
     for i in range(CHUNK):
-        cosines.append( math.cos( 440.0 * TIME * (2 * math.pi) ) )
-        sines.append( math.sin( 440.0 * TIME * (2 * math.pi) ) )
+        cosines.append( math.cos( FREQ * TIME * (2 * math.pi) ) )
+        sines.append( math.sin( FREQ * TIME * (2 * math.pi) ) )
         TIME += 1.0 / RATE
 
     # Listen for a tenth of a second
     received_signal = struct.unpack("f"*CHUNK, stream.read(CHUNK))
 
-    sine_amplitude = 0.0
-    cosine_amplitude = 0.0
-    for i in range(len(received_signal)):
-        cosine_amplitude += received_signal[ i ] * cosines[ i ] / len( received_signal )
-        sine_amplitude += received_signal[ i ] * sines[ i ] / len( received_signal )
-
-    mag = math.sqrt( cosine_amplitude**2 + sine_amplitude**2 ) * 1000
-    theta = math.degrees( math.atan2( cosine_amplitude, sine_amplitude ) )
-
+    # clear screen and draw axes
     context.foreground = bg
     pixmap.draw_rectangle(context, True, 0, 0, WIDTH, HEIGHT )
-
-    # draw axes
-
     context.foreground = axes
     pixmap.draw_line( context, int(WIDTH/2), 0, int(WIDTH/2), HEIGHT )
     pixmap.draw_line( context, 0, int(HEIGHT/2), WIDTH, int(HEIGHT/2) )
 
-    # draw signal
-    context.foreground = fg
-    y_coor = (HEIGHT/2) + (cosine_amplitude*8) * (HEIGHT/2)
-    x_coor = (WIDTH/2) + (sine_amplitude*8) * (WIDTH/2)
+    # Find amplitude of each bit
+    position_in_chunk = 0
+    for bit in range(CHUNK/BIT):
+        sine_amplitude = 0.0
+        cosine_amplitude = 0.0
+        for i in range(BIT):
+            cosine_amplitude += received_signal[ position_in_chunk ] * cosines[ position_in_chunk ] / BIT
+            sine_amplitude += received_signal[ position_in_chunk ] * sines[ position_in_chunk ] / BIT
+            position_in_chunk += 1
 
-    pixmap.draw_rectangle(context, True, int(x_coor)-5, int(y_coor)-5, 10, 10 )
+        mag = math.sqrt( cosine_amplitude**2 + sine_amplitude**2 ) * 1000
+        theta = math.degrees( math.atan2( cosine_amplitude, sine_amplitude ) )
+
+
+        # draw bit
+        context.foreground = fg
+        y_coor = (HEIGHT/2) + (cosine_amplitude*16) * (HEIGHT/2)
+        x_coor = (WIDTH/2) + (sine_amplitude*16) * (WIDTH/2)
+
+        pixmap.draw_rectangle(context, True, int(x_coor)-5, int(y_coor)-5, 10, 10 )
 
     # blit to screen
     drawable.draw_drawable( context, pixmap, 0, 0, 0, 0, -1, -1 )
--- test1.py
+++ test1.py
@@ -25,7 +25,7 @@ data = ""
 while True:
     data = ""
     while len(data) < CHUNK:
-        value = .25 * math.sin( 440.0 * TIME * (2 * math.pi) )
+        value = .25 * math.sin( 480.0 * TIME * (2 * math.pi) )
         data += struct.pack( 'f', value )
         TIME += 1.0 / RATE
 
