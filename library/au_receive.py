44cf8eff2ac665fc6d7874ad1a876d1fb2490c7a
Keith Winstein <keithw@mit.edu>
2011-08-16 02:15:00 -0400

    Working demo

--- library/au_receive.py
+++ library/au_receive.py
@@ -17,8 +17,21 @@ stream = p.open(format = FORMAT,
 
 TIME = 0 # seconds
 
+assert( abs( (float(SAMPLES_PER_SECOND) / float(CARRIER_CYCLES_PER_SECOND)) - (SAMPLES_PER_SECOND // CARRIER_CYCLES_PER_SECOND) ) < 0.01 )
+
+COS_CACHE = [0] * SAMPLES_PER_SECOND
+SIN_CACHE = [0] * SAMPLES_PER_SECOND
+for i in range( SAMPLES_PER_SECOND ):
+    COS_CACHE[ i ] = math.cos( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
+    SIN_CACHE[ i ] = math.sin( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
+    TIME += 1.0 / SAMPLES_PER_SECOND
+
+total_sample_count = 0
+
 def receive( num_samples ):
     global TIME
+    global total_sample_count
+
     sample_count = 0
     samples = []
     while sample_count < num_samples:
@@ -27,32 +40,38 @@ def receive( num_samples ):
         sample_count += SAMPLES_PER_CHUNK
 
     # Demodulate carrier
-    demodulated_samples = []
-    average_amplitude = complex( 0, 0 )
+    demodulated_samples = [0] * sample_count
+
+    imag = complex( 0, 1 )
 
     # Shift the modulated waveform back down to baseband
+    i = 0
     for s in samples:
-        I = s * math.cos( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
-        Q = s * math.sin( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
-        average_amplitude += complex( I, Q )
-        demodulated_samples.append( complex( I, Q ) )
-        TIME += 1.0 / SAMPLES_PER_SECOND
+        I = s * COS_CACHE[ total_sample_count % SAMPLES_PER_SECOND ]
+        Q = s * SIN_CACHE[ total_sample_count % SAMPLES_PER_SECOND ]
+        demodulated_samples[ i ] = complex( I, Q )
+        total_sample_count += 1
+        i += 1
 
     # calculate average amplitude (DC amplitude)
     # we will use this for auto-gain control
-    average_amplitude /= len(samples)
+    average_amplitude = sum( demodulated_samples ) / float(sample_count)
 
     # Shift samples in time back to original phase and amplitude (using carrier)
-    shifted_samples = [ .5 * (x / average_amplitude - 1) for x in demodulated_samples ]
+    shifted_samples = [ y.real for y in [ .5 * (x / average_amplitude - 1) for x in demodulated_samples ] ]
 
     # Low-pass filter
     window = SAMPLES_PER_SECOND // CARRIER_CYCLES_PER_SECOND
     last_samples = [0] * window
-    filtered_samples = []
-    for i in shifted_samples:
+    filtered_samples = [0] * sample_count
+    running_sum = sum( last_samples )
+    i = 0
+    for s in shifted_samples:
         last_samples.pop( 0 )
-        last_samples.append( i )
-        filtered_samples.append( sum( last_samples ) / window )
+        last_samples.append( s )
+        running_sum += s - last_samples[ 0 ]
+        filtered_samples[ i ] = running_sum / window
+        i += 1
 
     return filtered_samples
     
--- library/au_send.py
+++ library/au_send.py
@@ -17,9 +17,19 @@ stream = p.open(format = FORMAT,
 
 TIME = 0 # seconds
 
+COS_CACHE = [0] * SAMPLES_PER_SECOND
+SIN_CACHE = [0] * SAMPLES_PER_SECOND
+for i in range( SAMPLES_PER_SECOND ):
+    COS_CACHE[ i ] = math.cos( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
+    SIN_CACHE[ i ] = math.sin( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi )
+    TIME += 1.0 / SAMPLES_PER_SECOND
+
+total_sample_count = 0
+
 # Send one chunk of I samples, modulated onto the carrier frequency
 def send( samples ):
     global TIME
+    global total_sample_count
 
     sample_count = 0
     chunk_data = [ "" ]
@@ -28,8 +38,8 @@ def send( samples ):
     # Write payload
     
     for s in samples:
-        chunk_data[ chunk_number ] += struct.pack( 'f', ((s * AMPLITUDE) + DC) * math.cos( CARRIER_CYCLES_PER_SECOND * TIME * 2 * math.pi ) )
-        TIME += 1.0 / SAMPLES_PER_SECOND
+        chunk_data[ chunk_number ] += struct.pack( 'f', ((s * AMPLITUDE) + DC) * COS_CACHE[ total_sample_count % SAMPLES_PER_SECOND ] )
+        total_sample_count += 1
         sample_count += 1
 
         if sample_count == SAMPLES_PER_CHUNK:
