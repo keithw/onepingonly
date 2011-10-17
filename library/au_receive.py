d1623239cceb85c7b324854339f1a0bd385633db
Keith Winstein <keithw@mit.edu>
2011-10-17 05:57:44 -0400

    Make demodulator into class

--- library/au_receive.py
+++ library/au_receive.py
@@ -23,14 +23,9 @@ for i in range( cachelen ):
 COS_CACHE = numpy.array(COS_CACHE)
 SIN_CACHE = numpy.array(SIN_CACHE)
 
-PHASOR_CACHE = COS_CACHE + complex(0,1) * SIN_CACHE
-
-total_sample_count = 0
-
 num_amplitudes = 4096
-amplitudes = [ 1 ]
-samples_in_amplitude_history = 0
-amplitude_sum = sum( amplitudes )
+
+PHASOR_CACHE = COS_CACHE + complex(0,1) * SIN_CACHE
 
 nyquist_freq = float(SAMPLES_PER_SECOND) / 2.0
 
@@ -39,15 +34,8 @@ passband = float(CARRIER_CYCLES_PER_SECOND) / nyquist_freq
 tuner_numer, tuner_denom = scipy.signal.iirdesign( [ passband * 0.75 * 1.025, passband * 1.25 * 0.975 ],
                                                    [ passband * 0.75 * 0.975, passband * 1.25 * 1.025 ],
                                                    1, 40 )
-tuner_state = scipy.signal.lfiltic( tuner_numer, tuner_denom, [] )
 
 filter_numer, filter_denom = scipy.signal.iirdesign( passband * 0.5, passband, 3, 30 )
-filter_state = scipy.signal.lfiltic( filter_numer, filter_denom, [] )
-
-def receive( num_samples, stream, samples_per_chunk ):
-    factor = int( 1.0 / passband )
-    return decimate( demodulate( raw_receive( num_samples * factor, stream, samples_per_chunk ) ),
-                     factor )
 
 def decimate( samples, factor ):
     return samples[::factor]
@@ -67,67 +55,67 @@ def raw_receive( num_samples, stream, samples_per_chunk ):
     assert( sample_count == num_samples )
     return numpy.array(samples)
 
-def clear_amplitude_history():
-    global amplitudes
-    global amplitude_sum
-    global samples_in_amplitude_history
+class Demodulator:
+    def receive( self, num_samples, stream, samples_per_chunk ):
+        factor = int( 1.0 / passband )
+        return self.demodulate( raw_receive( num_samples * factor, stream, samples_per_chunk ) )[::factor]
 
-    amplitues = []
-    amplitude_sum = 0
-    samples_in_amplitude_history = 0
+    def __init__( self ):
+        self.total_sample_count = 0
+        self.amplitudes = [ 0 ]
+        self.amplitude_sum = 0
+        self.samples_in_amplitude_history = 0
 
-def demodulate( samples ):
-    global TIME
-    global total_sample_count
-    global amplitudes
-    global amplitude_sum
-    global samples_in_amplitude_history
+        self.tuner_state = scipy.signal.lfiltic( tuner_numer, tuner_denom, [] )
+        self.filter_state = scipy.signal.lfiltic( filter_numer, filter_denom, [] )
 
-    global tuner_state
-    global filter_state
+    def clear_amplitude_history( self ):
+        self.amplitudes = []
+        self.amplitude_sum = 0
+        self.samples_in_amplitude_history = 0
 
-    # Tune in band around carrier frequency
-    samples, tuner_state = scipy.signal.lfilter( tuner_numer, tuner_denom, samples, zi=tuner_state )
+    def demodulate( self, samples ):
+        # Tune in band around carrier frequency
+        samples, self.tuner_state = scipy.signal.lfilter( tuner_numer, tuner_denom, samples, zi=self.tuner_state )
 
-    sample_count = len( samples )
+        sample_count = len( samples )
 
-    # Shift the modulated waveform back down to baseband
-    i = 0
-    demodulated_samples = samples * numpy.roll( PHASOR_CACHE, total_sample_count )[0:sample_count]
-    total_sample_count += sample_count
+        # Shift the modulated waveform back down to baseband
+        demodulated_samples = samples * numpy.roll( PHASOR_CACHE, self.total_sample_count )[0:sample_count]
+        self.total_sample_count += sample_count
 
-    # calculate average amplitude (DC amplitude)
-    # we will use this for auto gain control
-    initializing = False
-    if samples_in_amplitude_history == 0: # initializing
-        initializing = True
-        samples_in_amplitude_history = sample_count # we assume same value each call
+        # calculate average amplitude (DC amplitude)
+        # we will use this for auto gain control
+        initializing = False
+        if self.samples_in_amplitude_history == 0: # initializing
+            initializing = True
+            self.samples_in_amplitude_history = sample_count # we assume same value each call
 
-    total_amplitude = sum( demodulated_samples )
-    amplitudes.append( total_amplitude )
-    amplitude_sum += total_amplitude
-    samples_in_amplitude_history += sample_count
+        total_amplitude = sum( demodulated_samples )
+        self.amplitudes.append( total_amplitude )
+        self.amplitude_sum += total_amplitude
+        self.samples_in_amplitude_history += sample_count
 
-    while samples_in_amplitude_history > num_amplitudes:
-        amplitude_sum -= amplitudes[ 0 ]
-        amplitudes.pop( 0 )
-        samples_in_amplitude_history -= sample_count
+        while self.samples_in_amplitude_history > num_amplitudes:
+            self.amplitude_sum -= self.amplitudes[ 0 ]
+            self.amplitudes.pop( 0 )
+            self.samples_in_amplitude_history -= sample_count
 
-    if initializing:
-        amplitude_overall_average = total_amplitude / sample_count
-    else:
-        amplitude_overall_average = amplitude_sum / samples_in_amplitude_history
+        if initializing:
+            amplitude_overall_average = total_amplitude / sample_count
+        else:
+            amplitude_overall_average = self.amplitude_sum / self.samples_in_amplitude_history
 
-    if amplitude_overall_average == 0:
-        amplitude_overall_average = 1
+        if amplitude_overall_average == 0:
+            amplitude_overall_average = 1
 
-    # Shift samples in time back to original phase and amplitude (using carrier)
-    constant = (DC/AMPLITUDE)/amplitude_overall_average
-    constant2 = DC/AMPLITUDE
-    shifted_samples = demodulated_samples * constant - constant2
+        # Shift samples in time back to original phase and amplitude (using carrier)
+        constant = (DC/AMPLITUDE)/amplitude_overall_average
+        constant2 = DC/AMPLITUDE
+        shifted_samples = demodulated_samples * constant - constant2
 
-    # Low-pass filter
-    filtered_samples, filter_state = scipy.signal.lfilter( filter_numer, filter_denom, shifted_samples, zi=filter_state )
-
-    return filtered_samples
+        # Low-pass filter
+        filtered_samples, self.filter_state = scipy.signal.lfilter( filter_numer, filter_denom, shifted_samples, zi=self.filter_state )
+        
+        return filtered_samples
     
